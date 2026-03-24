63c5b0f04bd3d2759301f38818f6855f5315ac39
ashish4hub <ashish.k620045@gmail.com>
2026-03-25 03:26:28 +0530

    Fixed structure

--- main.c
+++ main.c
@@ -1,25 +1,25 @@
-#include "uart/uart_tx.h"
-#include "uart/uart_rx.h"
+#include "uart/uart.h"
 #include "timer/timer.h"
 #include <string.h>
 #include <avr/interrupt.h>
 #include <stdint.h>
 int main(void){
-    uart_init();
-    rx_ini();
-    timer_config();
+    uart_init(9600);
+    timer_ini();
+    uint32_t wait = 0;  // timestamp
     DDRB |= (1 << PB5);
     DDRB |= (1 << PB4);
     sei();
+
     while(1){
         uart_print("hello world!\r\n");
         if(read_line()){
-            if (strcmp(line, "hello"))
+            if (strcmp(line, "hello") == 0)
             {
                 uart_print("uart setup done!");
                 PORTB |= (1 << PB5);
                 PORTB |= (1 << PB4);
-                if(nb_wait_ms(1000)){
+                if(nb_wait_ms(&wait,1000)){
                     PORTB &= ~(1 << PB5);
                     PORTB &= ~(1 << PB4);
                 }
--- uart/uart.h
+++ uart/uart.h
@@ -10,11 +10,10 @@
 
  void uart_put_tx(char c);
  void uart_print(const char *s);
- void uart_init(void);
+ void uart_init(uint32_t BAUD);
  int8_t uart_rx_avail(void);
  char uart_get_data(void);
  uint8_t read_line(void);
- void rx_ini(void);
 
  extern volatile uint8_t Tx_buff[Tx_buffer_size];
  extern volatile uint8_t tx_tail;
