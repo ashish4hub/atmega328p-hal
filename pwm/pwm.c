df59cff7d97bcfe8877ecb14c6fac94097aee236
ashish4hub <ashish.k620045@gmail.com>
2026-03-29 00:37:07 +0530

    Modified APIsfor channel selection, frequency and dutycycle

--- pwm/pwm.c
+++ pwm/pwm.c
@@ -10,16 +10,19 @@ typedef enum {
     pwm_CH1B   /* PB2 */
 } pwm_channel_t;
 static uint16_t pwm_top;
-void pwm_init(pwm_channel_t chnl, uint32_t freq){
+void pwm_init(uint32_t freq){
     TCCR1A = (1 << WGM11);
     TCCR1B = (1 << WGM12) | (1 << WGM13) | (1 << CS11);
     pwm_top = (F_CPU / (8UL * freq)) - 1;
     ICR1 = pwm_top;
-    if(chnl == pwm_CH1A){
+}
+void pwm_enb_channel(pwm_channel_t channel){
+    if(channel == pwm_CH1A){
         DDRB |= (1 << PB1);
         TCCR1A |= (1 << COM1A1);
     }
-    else if (chnl == pwm_CH1B){
+    else if (channel == pwm_CH1B)
+    {
         DDRB |= (1 << PB2);
         TCCR1A |= (1 << COM1B1);
     }
@@ -29,11 +32,11 @@ void pwm_set(pwm_channel_t ch, uint32_t duty_percent){   /*PWM channel and duty 
     if(duty_percent > 100) duty_percent = 100; 
     
     uint16_t duty = (pwm_top * duty_percent) / 100;    /*Duty percentage to OCRx value conversion*/
-
+    
     if(ch == pwm_CH1A){
         OCR1A = duty;
     }
-    else if(ch == pwm_CH1B){
-        OCR1B = duty;
+    else if (ch == pwm_CH1B){
+        OCR1B =  duty;
     }
 }
--- pwm/pwm.h
+++ pwm/pwm.h
@@ -7,7 +7,8 @@ typedef enum {
     pwm_CH1B   /* PB2 */
 }pwm_channel_t;
 
-void pwm_init(pwm_channel_t chnl, uint32_t freq);  /* Initialize pwm with desired frequency and PIN */
+void pwm_init(uint32_t freq);  /* Initialize pwm with desired frequency and PIN */
+void pwm_enb_channel(pwm_channel_t channel);      /* Channel configuration */
 void pwm_set(pwm_channel_t ch, uint32_t duty_percent);     /* Set pin (PB1 or PB2), set duty cycle (0 --> 1000)*/
 
 #endif
