6d2ff46a2915762b69a103c1252eaf411ba97765
ashish4hub <ashish.k620045@gmail.com>
2026-03-31 00:39:10 +0530

    Added TIMER2 PWM

--- pwm/pwm.c
+++ pwm/pwm.c
@@ -1,4 +1,4 @@
-/* TIMER1 PWM driver */
+/* PWM Driver TIMER1 & TIMER2 */
 /* MCU = ATmega328P */
 
 #define F_CPU 16000000UL
@@ -6,17 +6,30 @@
 #include<avr/interrupt.h>
 #include<stdint.h>
 
-static uint8_t ch1a_enb = 0;
-static uint8_t ch1b_enb = 0;
+static uint8_t ch1a_enb = 0;          /* TIMER1 channle A enable flag */
+static uint8_t ch1b_enb = 0;         /* TIMER1 channle B enable flag */
+static uint8_t ch2a_enb = 0;        /* TIMER2 channle A enable flag */
+static uint8_t ch2b_enb = 0;       /* TIMER2 channle B enable flag */
+
+static uint16_t pwm_top;   /*  ICR1 (TIMER1 TOP value) */
+static uint8_t pwm_top2;  /* OCRA (TIMER2 TOP vlaue) */
+
 
 /* Channels */
 typedef enum {
     pwm_CH1A,  /* PB1 */
-    pwm_CH1B   /* PB2 */
+    pwm_CH1B,   /* PB2 */
+    pwm_CH2A,
+    pwm_CH2B
 } pwm_channel_t;
 
+/*TIMER selection for PWM initialization */
+typedef enum {
+    pwm_TIMER1,
+    pwm_TIMER2
+} pwm_timer_t;
 
-/* Prescaler table */
+/* TIMER1 Prescaler table */
 typedef struct {
     uint16_t prescaler_val;
     uint8_t cs_bits;
@@ -30,7 +43,18 @@ const prescaler_t prescalers[] = {
     {1024, (1 << CS12) | (1 << CS10)}
 };
 
-/* Computation fucntion for Prescaler */
+/* TIMER2 (8 bits) prescaler table */
+const prescaler_t prescalers2 [] = {
+    {1,(1 << CS20)},
+    {8, (1 << CS21)},
+    {32, (1 << CS21) | (1 << CS20)},
+    {64, (1 << CS22)},
+    {128, (1 << CS22) | (1 << CS20)},
+    {256, (1 << CS22) | (1 << CS21)},
+    {1024, (1 << CS22) | (1 << CS21) | (1 << CS20)}
+};
+
+/* Computation fucntion for TIMER1 Prescaler */
 uint8_t pwm_compute(uint32_t freq, uint16_t *top, uint8_t *cs_bits){
 
     /* Selection of minimum prescaler */
@@ -46,15 +70,29 @@ uint8_t pwm_compute(uint32_t freq, uint16_t *top, uint8_t *cs_bits){
     return 0;    // Frequency not achievable
 }
 
-static uint16_t pwm_top;   /*  ICR1 */
+/* TIMER2 pwm compute */
+uint8_t pwm2_compute(uint32_t freq, uint8_t *top2, uint8_t *cs_bits2){
+    for(uint8_t i = 0; i < 7; i++){
+        uint32_t temp = (F_CPU / (prescalers2[i].prescaler_val * freq)) - 1;
 
-void pwm_init(uint32_t freq){    /* PWM Initialisation and TOP (ICR1) calculation based on passed frequency */
-    if(freq == 0){               /* Frequency edge case */
-        return;
+        if(temp <= 255){
+            *top2 = (uint8_t)temp;
+            *cs_bits2 = prescalers2[i].cs_bits;
+            return 1;  // Success
+        }
     }
+    return 0; // Failed
+}
+
+/* TIMER1 configuration for PWM (fast pwm mode) */
+void timer1_pwm_init(uint32_t freq){
+
+    // Clearing channel1 flag on initialization
     ch1a_enb = 0;
     ch1b_enb = 0;
-    uint8_t cs_bits;
+
+    uint8_t cs_bits;  // Computed TIMER1 Prescaler bits
+
     if(!pwm_compute(freq, &pwm_top, &cs_bits)){
         return;;
     }
@@ -63,7 +101,25 @@ void pwm_init(uint32_t freq){    /* PWM Initialisation and TOP (ICR1) calculatio
     ICR1 = pwm_top;
 }
 
-/*PWM channel (pin) selection */
+/* TIMER2 configuration for pwm (fast pwm mode) */
+void timer2_pwm_init(uint32_t freq){
+
+    // Clearing channel2 flag on initialization
+    ch2a_enb = 0;
+    ch2b_enb = 0;
+
+    uint8_t cs_bits2;         // Computed TIMER2 Prescaler bits
+    
+    if(!pwm2_compute(freq, &pwm_top2, &cs_bits2)){
+        return;
+    }
+    TCCR2A = (1 << WGM20) | (1 << WGM21);
+    TCCR2B = (1 << WGM22) | cs_bits2;
+    OCR2A = pwm_top2;
+}
+
+
+/* TIMER1 PWM channel (pin) selection */
 void pwm_enb_channel(pwm_channel_t channel){
 
     if(channel == pwm_CH1A){
@@ -83,19 +139,76 @@ void pwm_enb_channel(pwm_channel_t channel){
     }
 }
 
-/* Dutycycle (percent) assignment according to channel */
-void pwm_set(pwm_channel_t ch, uint32_t duty_percent){
+/* TIMER2 PWM channel (pin) selection */
+void pwm2_enb_chnl(pwm_channel_t chnl2){
 
+    if(chnl2 == pwm_CH2A){
+        if(!ch2a_enb){
+            DDRD |= (1 << PD7);
+            TCCR2A |= (1 << COM2A1);
+            ch2a_enb = 1;
+        }
+    }
+    else if (chnl2 == pwm_CH2B){
+        if(!ch2b_enb){
+            DDRD |= ( 1<< PD3);
+            TCCR2A |= (1 << COM2B1);
+            ch2b_enb = 1;
+        }
+    }
+}
+
+/* TIMER1 PWM dutycycle assignment */
+void timer1_pwm_set(pwm_channel_t ch, uint8_t duty_percent){
+    
     pwm_enb_channel(ch);
     
     if(duty_percent > 100) duty_percent = 100; 
     
-    uint16_t duty = (pwm_top * duty_percent) / 100;    /* Duty percentage to OCR1x value conversion */
+    uint16_t duty = ((uint32_t)pwm_top * duty_percent) / 100;    /* Duty percentage to OCR1x value conversion */
     
     if(ch == pwm_CH1A){
         OCR1A = duty;
     }
     else if (ch == pwm_CH1B){
         OCR1B =  duty;
+    }
+}
+
+
+/* TIMER2 PWM dutycycle assignment */
+void timer2_pwm_set(pwm_channel_t ch, uint8_t duty_percent){
+    
+    pwm2_enb_chnl(ch);
+
+    if(duty_percent > 100) duty_percent = 100;
+
+    uint8_t duty = ((uint32_t)pwm_top2 * duty_percent) / 100;
+
+    OCR2B = duty;   // Only CH2B Supported in variable frequency mode
+}
+
+void pwm_init(uint32_t freq, pwm_timer_t timer){    /* PWM Initialisation and TOP (ICR1) calculation based on passed frequency */
+    
+    if(freq == 0){               /* Frequency edge case */
+        return;
+    }
+    if(timer == pwm_TIMER1){
+        timer1_pwm_init(freq);
+    }
+    else if(timer == pwm_TIMER2){
+        timer2_pwm_init(freq);
+    }
+
+}
+
+/* Dutycycle (percent) assignment according to channel */
+void pwm_set(pwm_channel_t ch, uint32_t duty_percent){
+    
+    if(ch == pwm_CH1A || ch == pwm_CH1B){
+        timer1_pwm_set(ch, duty_percent);
+    }
+    else if (ch == pwm_CH2A || ch == pwm_CH2B){
+        timer2_pwm_set(ch, duty_percent);
     }
 }
