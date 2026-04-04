53aeedcf8b732712f67c7082c5dba362013705c6
ashish4hub <ashish.k620045@gmail.com>
2026-04-04 15:26:54 +0530

    Added reference, mode and prescaler selection

--- adc/adc.c
+++ adc/adc.c
@@ -10,28 +10,101 @@
 #include <avr/interrupt.h>
 #include <stdint.h>
 
+static volatile uint16_t adc_result = 0;    // Storing ADC reading
+static volatile uint8_t adc_done = 0;      // Flag
+
+/* ADC references*/
+typedef enum {
+ADC_REF_AREF = 0x00,
+ADC_REF_VCC = 0x01,
+ADC_REF_INTERNAL = 0x03
+}ADC_REF_t;
+
+/* ADC mode selection */
+typedef enum{
+ADC_POLLING,
+ADC_INTERRUPT
+}ADC_MODE_t;
+
+/* ADC prescaler */
+typedef enum{
+ADC_PS_2 = 0x01,
+ADC_PS_4 = 0x02,
+ADC_PS_8 = 0x03,
+ADC_PS_16 = 0x04,
+ADC_PS_32 = 0x05,
+ADC_PS_64 = 0x06,
+ADC_PS_128 = 0x07
+}ADC_prescaler_t;
+
 /* ADC Channels */
 typedef enum {
-    ADC_CH0,
-    ADC_CH1,
-    ADC_CH2,
-    ADC_CH3,
-    ADC_CH4,
-    ADC_CH5
+    ADC_CH0 = 0x00,
+    ADC_CH1 = 0x01,
+    ADC_CH2 = 0x02,
+    ADC_CH3 = 0x03,
+    ADC_CH4 = 0x04,
+    ADC_CH5 = 0x05
 } ADC_Channels_t;
 
-void adc_init(void){
-    ADMUX |= (1 << REFS0);
-    ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+/* Configuration */
+ typedef struct{
+    ADC_REF_t reference;
+    ADC_prescaler_t prescaler;
+    ADC_MODE_t mode;
+}ADC_config_t;
 
+static ADC_MODE_t adc_mode;
+
+/* ADC Initialization */
+void adc_init(const ADC_config_t *config){
+
+    adc_mode = config->mode;               // Store Mode
+
+    /*-------------------ADMUX-------------------*/
+    ADMUX = 0;
+    ADMUX |= (config->reference << REFS0);
+
+    /*-------------------ADCSRA-------------------*/
+    ADCSRA = 0;
+    ADCSRA |= (1 << ADEN);   // Enable ADC
+    ADCSRA |= (config->prescaler);      // Set prescaler bits
+
+    if(config->mode == ADC_INTERRUPT){
+        ADCSRA |= (1 << ADIE);      // ADC interrupt enable
+        sei();                //Enable global interrupt
+    }
 }
 
-/* Channel selection and reading */
-uint16_t adc_read(ADC_Channels_t channel){
+ /* ADC reading */
+ void ADC_start(ADC_Channels_t channel){
+    if(ADCSRA & (1 << ADSC)){
+        return;
+    }
+    adc_done = 0;
+    ADMUX = (ADMUX & 0xF0) | (channel & 0x0F);
+    ADCSRA |= (1 << ADSC);
 
-    ADMUX = (ADMUX & 0xF0) | (channel & 0x0F);     // Channel config
-    ADCSRA |= (1 << ADSC);                        // start conversion
-    while(ADCSRA & (1 << ADSC));                 // wait until conversion is complete (polling)
-    ADCSRA |= (1 << ADIF);                      // Clear flag
-    return ADC;                                // return ADC converted value
+    if(adc_mode == ADC_POLLING){
+        while(ADCSRA & (1 << ADSC));
+        adc_result = ADC;
+        adc_done = 1;    
+    }
 }
+
+/* ADC status API */
+uint8_t ADC_done(void){
+    return adc_done;
+}
+
+/* ADC result API */
+uint16_t ADC_get_result(void){
+    return adc_result;
+}
+
+/* ADC interrupt function*/
+ISR(ADC_vect){
+    adc_result = ADC;
+    adc_done = 1;
+}
+
