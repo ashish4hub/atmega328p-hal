22dcf64df970db49cce9e36e7233cbd518d47167
ashish4hub <ashish.k620045@gmail.com>
2026-04-04 15:27:29 +0530

    Modified public APIs

--- adc/adc.h
+++ adc/adc.h
@@ -3,17 +3,52 @@
 
 #include <stdint.h>
 
+/* ADC references*/
+typedef enum {
+ADC_REF_AREF = 0x00,
+ADC_REF_VCC = 0x01,
+ADC_REF_INTERNAL = 0x03
+}ADC_REF_t;
+
+/* ADC mode selection */
+typedef enum{
+ADC_POLLING,
+ADC_INTERRUPT
+}ADC_MODE_t;
+
+/* ADC prescaler */
+typedef enum{
+ADC_PS_2 = 0x01,
+ADC_PS_4 = 0x02,
+ADC_PS_8 = 0x03,
+ADC_PS_16 = 0x04,
+ADC_PS_32 = 0x05,
+ADC_PS_64 = 0x06,
+ADC_PS_128 = 0x07
+}ADC_prescaler_t;
+
+
 /* ADC Channels */
 typedef enum {
-    ADC_CH1,
-    ADC_CH2,
-    ADC_CH3,
-    ADC_CH4,
-    ADC_CH5,
-    ADC_CH6
+    ADC_CH0 = 0x00,
+    ADC_CH1 = 0x01,
+    ADC_CH2 = 0x02,
+    ADC_CH3 = 0x03,
+    ADC_CH4 = 0x04,
+    ADC_CH5 = 0x05
 } ADC_Channels_t;
 
-void adc_init(void);
-uint16_t adc_read(ADC_Channels_t channel);
+/* Configuration */
+ typedef struct{
+    ADC_REF_t reference;
+    ADC_prescaler_t prescaler;
+    ADC_MODE_t mode;
+}ADC_config_t;
+
+
+void adc_init(const ADC_config_t *config);
+void ADC_start(ADC_Channels_t channel);
+uint8_t ADC_done(void);
+uint16_t ADC_get_result(void);
 
 #endif
