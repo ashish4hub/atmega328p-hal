541317a2e9f0e1e9487598f7439a6bd1326320aa
ashish4hub <ashish.k620045@gmail.com>
2026-04-09 00:21:18 +0530

    Fixing Race condition

--- uart/uart.c
+++ uart/uart.c
@@ -1,11 +1,21 @@
+/* UART driver */
+/* Hardware abstraction layer for serial communicattion using USART */
+/* MCU = ATmega328P */
+
 #define F_CPU 16000000UL
 #include<avr/io.h>
 #include<avr/interrupt.h>
 #include<stdint.h>
 
-#define Tx_buffer_size 64
+#define Tx_buffer_size 64  // TX Buffer size
+#define rx_buff_size 64   // RX Buffer size
 
-#define rx_buff_size 64   // Buffer size
+#define line_size 32      // Line size for RX string
+
+char line[line_size];     // Character array for forming string
+
+uint8_t line_idx = 0;     // Line array index
+
 
 volatile char rx_buff[rx_buff_size];
 volatile uint8_t rx_head = 0;
@@ -15,7 +25,9 @@ volatile uint8_t Tx_buff[Tx_buffer_size];
 volatile uint8_t tx_tail = 0;
 volatile uint8_t tx_head = 0;
 
+/* Store characters in TX buffer and enables TX interrupt */
 void uart_put_tx(char c){
+
   uint8_t next = (tx_head +1 ) % Tx_buffer_size;
   
 //drops character if buffer is full
@@ -26,11 +38,15 @@ void uart_put_tx(char c){
   tx_head = next;
   UCSR0B |= (1 << UDRIE0);
 }
+
+/* UART print API */
 void uart_print(const char *s){
   while(*s){
     uart_put_tx(*s++);
   }
 }
+
+/* USART initialization */
 void uart_init(uint32_t BAUD){
   uint16_t BRC = ((F_CPU / (16UL * BAUD)) - 1UL);
   UBRR0H = (BRC >> 8);
@@ -38,7 +54,8 @@ void uart_init(uint32_t BAUD){
   UCSR0B = (1 << TXEN0) | (1 << RXCIE0) | (1 << RXEN0);
   UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
 }
-//TX (UDRE) interrupt function 
+
+/* TX (UDRE) interrupt function */ 
 ISR(USART_UDRE_vect)
 {
     if (tx_head == tx_tail) {
@@ -48,6 +65,8 @@ ISR(USART_UDRE_vect)
         tx_tail = (tx_tail + 1) % Tx_buffer_size;
     }
 }
+
+/* RX interrupt function */
 ISR(USART_RX_vect){
   char data = UDR0; // Read received byte in data 
   uint8_t next = (rx_head + 1) % rx_buff_size;
@@ -58,19 +77,21 @@ ISR(USART_RX_vect){
     rx_head = next;
   }
 }
+
+/* If data is availabe for reading */
 uint8_t uart_rx_avail(void){
   return (rx_head != rx_tail);
 }
+
+/* Get RX data */
 char uart_get_data(void){
   if(rx_head == rx_tail) return 0;
   char data = rx_buff[rx_tail];
   rx_tail = (rx_tail + 1) % rx_buff_size;
   return data;
 }
-//Building line 
-#define line_size 32
-char line[line_size];
-uint8_t line_idx = 0;
+
+/* Building Line (string) */
 uint8_t read_line(void){
   while(uart_rx_avail()){
     char c = uart_get_data();
@@ -85,4 +106,3 @@ uint8_t read_line(void){
   }
   return 0;
 }
-
--- uart/uart.h
+++ uart/uart.h
@@ -7,19 +7,10 @@
 #define Rx_buffer_size 64
 #define line_size 32
 
- void uart_put_tx(char c);
- void uart_print(const char *s);
  void uart_init(uint32_t BAUD);
- int8_t uart_rx_avail(void);
- char uart_get_data(void);
+ void uart_print(const char *s);
  uint8_t read_line(void);
 
- extern volatile uint8_t Tx_buff[Tx_buffer_size];
- extern volatile uint8_t tx_tail;
- extern volatile uint8_t tx_head;
- extern volatile uint8_t rx_buff[Tx_buffer_size];
- extern volatile uint8_t rx_tail;
- extern volatile uint8_t rx_head;
  extern const char line[line_size];
 
  #endif
