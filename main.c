23d77abbf1e22c96fa6cc3b8df86f7070f032ef4
ashish4hub <ashish.k620045@gmail.com>
2026-04-11 12:42:40 +0530

    Changed driver implementation

--- main.c
+++ main.c
@@ -6,23 +6,16 @@
 #include <string.h>
 #include <avr/interrupt.h>
 #include <stdint.h>
+#include <util/delay.h>
+
+#define adc_led PB4
 
 int main(void){
 
-    uart_init(9600);                 // UART initialization at 9600 BUAD 
-    pwm_init(1000,pwm_TIMER1);      // Frequency = 1000 and TIMER1 PWM initialization
-    pwm_init(5000,pwm_TIMER2);     // Frequency = 5000 and TIMER2 PWM initialization
+    USART_init(9600);                 // UART initialization at 9600 BUAD 
+    pwm_init(1000,pwm_TIMER1);       // Frequency = 1000 and TIMER1 PWM initialization
+    pwm_init(5000,pwm_TIMER2);      // Frequency = 5000 and TIMER2 PWM initialization
     timer_init();                  // TIMER0 initialization for timing (counting)
-
-    /* Time stamps for timing logic in pwm and timer driver */  
-    uint32_t led1_wait = get_ticks();
-    uint32_t led2_wait = get_ticks();
-    uint32_t led3_wait = get_ticks();
-
-    /* Duty cycle variables for led */
-    uint8_t led1_duty = 0;
-    uint8_t led2_duty = 0;
-    uint8_t led3_duty = 0;
 
     /* ADC configuration */
     ADC_config_t config = {
@@ -31,39 +24,39 @@ int main(void){
         .prescaler = ADC_PS_128
     };
     
-    ADC_init(&config);
+    ADC_init(&config);                     // ADC configuration
+    uint16_t ADC_result = 0;
+    
+    DDRB |= (1 << adc_led);     // ADC alert led
 
+    /* Time stamps for timing logic in pwm and timer driver */  
+    uint32_t led1_wait = get_ticks();
+    uint32_t adc_wait = get_ticks();
+
+    /* Duty cycle variables for led */
+    uint8_t led1_duty = 0;
     sei();
 
     while(1){
-
-        if(nb_wait_ms(&led1_wait,20)){
-            pwm_set(pwm_CH1A,led1_duty);    /* Set duty cycle for pwm pin */
-
+        if(nb_wait_ms(&led1_wait,50)){
+            pwm_set(pwm_CH1A,led1_duty);
             led1_duty++;
 
             if(led1_duty > 100){
                 led1_duty = 0;
             }
         }
-
-        if(nb_wait_ms(&led2_wait,40)){
-            pwm_set(pwm_CH1B,led2_duty);
-
-            led2_duty++;
-
-            if(led2_duty > 100){
-                led2_duty = 0;
-            }
+        if(nb_wait_ms(&adc_wait,10)){
+            ADC_start(ADC_CH0);
         }
-
-        if(nb_wait_ms(&led3_wait,60)){
-            pwm_set(pwm_CH2B, led3_duty);
-
-            led3_duty++;
-
-            if(led3_duty > 100){
-                led3_duty = 0;
+        if(ADC_done()){
+            ADC_result = ADC_get_result();
+            
+            if(ADC_result > 513){
+                PORTB |= (1 << adc_led);
+            }
+            else {
+                PORTB &= ~(1 << adc_led);
             }
         }
     }
