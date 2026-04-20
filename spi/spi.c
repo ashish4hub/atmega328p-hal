974561818e9852d39dc61408cc59b5c56e903adb
ashish4hub <ashish.k620045@gmail.com>
2026-04-21 01:27:34 +0530

    ..

--- spi/spi.c
+++ spi/spi.c
@@ -4,13 +4,14 @@
 
 
 /* PINS 
-* PB3 ----> MOSI (OUTPUT)
-* PB4 ----> MISO (INPUT)
-* PB2 ----> SS (OUTPUT)
-* PB5 -----> SCK (OUTPUT)
+* PB3 ----> MOSI (OUTPUT) ----> Master Output Slave In
+* PB4 ----> MISO (INPUT) ----> Master In Slave Output
+* PB2 ----> SS (OUTPUT) ----> Slave Select (MASTER)
+* PB5 ----> SCK (OUTPUT) ----> Clock
 */
 
 #include "spi.h"
+
 
 /* SPI initialization */
 void SPI_init(void){
@@ -19,10 +20,10 @@ void SPI_init(void){
     DDRB &= ~ (1 << PB4);                             // MISO as input
 
     SPCR = (1 << SPE) | (1 << MSTR) | (1 << SPR1);          // SPI eanble as Master with F_CPU/64 clock
-    SPCR &= ~((1 << CPOL) | (1 << CPHA));     // leading edge --> Risisng | sampling edge
+    SPCR &= ~((1 << CPOL) | (1 << CPHA));     // leading edge --> Risisng, sampling edge
 }
 
-/* SPI Trnasfer function */
+/* SPI Transfer function */
 uint8_t SPI_transfer(uint8_t data){
     SPDR = data;
 
