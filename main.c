cd9e72c1fcbc88bf2ea0c345fa2b75745443bd6e
ashish4hub <ashish.k620045@gmail.com>
2026-04-23 00:49:47 +0530

    Cleaned main

--- main.c
+++ main.c
@@ -1,25 +1,19 @@
-/* AVR drivers application */
 /* CLI  based system */
-
-
 
 #define F_CPU 16000000UL
 
 #include "CLI/CLI.h"
 #include <avr/interrupt.h>
 
-#define adc_led PB0      // Led 
-
 int main(void){
 
     USART_init(9600);                 // UART initialization at 9600 BUAD 
     pwm_init(1000,pwm_TIMER1);       // Frequency = 1000 and TIMER1 PWM initialization
-    timer_init();                  // TIMER0 initialization for timing (counting)
+    timer_init();                   // TIMER0 initialization for timing (counting)
+    
+    sei();                         // Global interrupt enable
 
-    sei();        // Global interrupt enable
-
-    /* Initial message */
-    USART_print("Type start\n");
+    USART_print("Enter commands\n");
 
     while(1){
         
