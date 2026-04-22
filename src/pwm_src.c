6fee6cc281c7afd47154b4c174ebd1fc5484f49e
ashish4hub <ashish.k620045@gmail.com>
2026-04-23 00:44:39 +0530

    Fixed declaration error

--- src/pwm_src.c
+++ src/pwm_src.c
@@ -2,10 +2,10 @@
 
 #include "pwm_src.h"
 
-pwm_state_t pwm_state = state_pwm_off;
+pwm_state_t pwm_state = state_ramp_off;
 
 /* Start led ram up */
-void run_pwm(void){
+void run_led_rampup(void){
 
     static uint32_t duty_wait = 0;
     static uint8_t duty = 0;
@@ -25,16 +25,45 @@ void run_pwm(void){
     }
 }
 
+/* LED fade */
+void led_fade(void){
+
+    static uint32_t led_fade_wait = 0;
+    static uint8_t duty = 0;
+    static int8_t step = 1;
+
+    if(led_fade_wait == 0){
+        led_fade_wait = get_ticks();
+    }
+
+    if(nb_wait_ms(&led_fade_wait,10)){
+
+        duty += step;
+
+        if(duty >= 100){
+            duty = 100;
+            step = -1;
+        }
+        else if(duty == 0){
+            step = 1;
+        }
+        pwm_set(pwm_CH1A,duty);
+    }
+}
+
 /* State based control */
 void pwm_set_state(pwm_state_t state){
 
     switch (state)
     {
-    case state_pwm_on:
-        pwm_state = state_pwm_on;
+    case state_ramp_on:
+        pwm_state = state_ramp_on;
         break;
-    case state_pwm_off:
-    pwm_state = state_pwm_off;
+    case state_ramp_off:
+    pwm_state = state_ramp_off;
+        break;
+    case state_led_fade:
+        pwm_state = state_led_fade;
         break;
     }
 }
@@ -44,14 +73,18 @@ void pwm_src(void){
     
     switch (pwm_state)
     {
-    case state_pwm_on:
+    case state_ramp_on:
         pwm_start(pwm_CH1A);
-        run_pwm();
+        run_led_rampup();
         break;
     
-    case state_pwm_off:
+    case state_ramp_off:
         pwm_disable(pwm_CH1A);
         break;
+    
+    case state_led_fade:
+        pwm_start(pwm_CH1A);
+        led_fade();
     }
 }
 
