164a7f25c8771af3bcfe6ffdb9c37b2fc4696f02
ashish4hub <ashish.k620045@gmail.com>
2026-09-17 22:59:44 +0530

    Renamed ADC application module

---
+++ src/moist.h
@@ -0,0 +1,26 @@
+#ifndef ADC_SRC_H
+#define ADC_SRC_H
+
+#include "../driver/adc/adc.h"
+#include <stdint.h>
+
+/* Work state */
+typedef enum{
+    MOIST_start_detect,
+    MOIST_stop_detect
+}MOIST_work_state;
+
+/* GAS reading status */
+typedef enum{
+    MOIST_idle,
+    MOIST_high,
+    MOIST_low
+}MOIST_reading_t;
+
+
+void MOIST_set_state(MOIST_work_state st);                  // set state to start or stop adc according to command
+void MOIST_src(void);                                      // Reading adc
+MOIST_reading_t MOIST_status(void);                       // Return gas status (High or Low)
+uint16_t MOIST_result(void);                             // Return conversion result
+
+#endif
